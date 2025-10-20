34da418e07376c81e558bcead22f2ba0d87fa03d
Freyder Urbano <freurbano@gmail.com>
2025-10-20 10:44:38 -0500

    Creacion nuevo modelo NuevaClase

---
+++ ApiTienda2025/Models/NuevaClase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiTienda2025.Models
+{
+    public class NuevaClase
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
